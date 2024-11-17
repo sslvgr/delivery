d1b2def3f02e0835cb27fba0705f3617c13a38b3
i.suslov@teez.kz <i.suslov@teez.kz>
2024-11-17 15:19:39 +0500

    first commit

---
+++ Utils/Primitives/Aggregate.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Primitives;
+
+public abstract class Aggregate : Entity<Guid>, IAggregateRoot
+{
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    protected Aggregate(Guid id) : base(id)
+    {
+    }
+
+    protected Aggregate()
+    {
+    }
+
+    public IReadOnlyCollection<DomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
+
+public interface IAggregateRoot;
