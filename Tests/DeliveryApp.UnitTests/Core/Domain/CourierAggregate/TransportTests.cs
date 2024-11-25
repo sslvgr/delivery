a16a84d1975c22c2fd838dca871016f06eb4c652
Igor Suslov <98566051+sslvgr@users.noreply.github.com>
2024-11-25 11:50:05 +0500

    Module-3 (#3)

---
+++ Tests/DeliveryApp.UnitTests/Core/Domain/CourierAggregate/TransportTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using DeliveryApp.Core.Domain.CourierAggregate;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Core.Domain.CourierAggregate;
+
+public class TransportTests
+{
+    public static IEnumerable<object[]> GetTransports()
+    {
+        yield return [Transport.Pedestrian, "pedestrian"];
+        yield return [Transport.Bicycle, "bicycle"];
+        yield return [Transport.Car, "car"];
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTransports))]
+    public void ReturnCorrectIdAndName(Transport transport, string name)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        transport.Name.Should().Be(name);
+    }
+
+    [Theory]
+    [InlineData(1, "pedestrian")]
+    [InlineData(2, "bicycle")]
+    [InlineData(3, "car")]
+    public void CanBeFoundById(int id, string name)
+    {
+        //Arrange
+
+        //Act
+        var status = Transport.FromId(id).Value;
+
+        //Assert
+        status.Id.Should().Be(id);
+        status.Name.Should().Be(name);
+    }
+
+    [Theory]
+    [InlineData("pedestrian")]
+    [InlineData("bicycle")]
+    [InlineData("car")]
+    public void CanBeFoundByName(string name)
+    {
+        //Arrange
+
+        //Act
+        var result = Transport.FromName(name);
+
+        //Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be(name);
+    }
+
+    [Fact]
+    public void ReturnErrorWhenTransportNotFoundById()
+    {
+        //Arrange
+        var id = -1;
+
+        //Act
+        var result = Transport.FromId(id);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnErrorWhenStatusNotFoundByName()
+    {
+        //Arrange
+        var name = "not-existent-name";
+
+        //Act
+        var result = Transport.FromName(name);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnListOfStatuses()
+    {
+        //Arrange
+
+        //Act
+        var allStatuses = Transport.List();
+
+        //Assert
+        allStatuses.Should().NotBeEmpty();
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Core/Domain/OrderAggregate/OrderStatusTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using DeliveryApp.Core.Domain.OrderAggregate;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Core.Domain.OrderAggregate;
+
+public class OrderStatusTests
+{
+    public static IEnumerable<object[]> GetOrderStatuses()
+    {
+        yield return [OrderStatus.Created, "created"];
+        yield return [OrderStatus.Assigned, "assigned"];
+        yield return [OrderStatus.Completed, "completed"];
+    }
+
+    [Theory]
+    [MemberData(nameof(GetOrderStatuses))]
+    public void ReturnCorrectIdAndName(OrderStatus orderStatus, string name)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        orderStatus.Name.Should().Be(name);
+    }
+
+    [Theory]
+    [InlineData(1, "created")]
+    [InlineData(2, "assigned")]
+    [InlineData(3, "completed")]
+    public void CanBeFoundById(int id, string name)
+    {
+        //Arrange
+
+        //Act
+        var status = OrderStatus.FromId(id).Value;
+
+        //Assert
+        status.Id.Should().Be(id);
+        status.Name.Should().Be(name);
+    }
+
+    [Theory]
+    [InlineData("created")]
+    [InlineData("assigned")]
+    [InlineData("completed")]
+    public void CanBeFoundByName(string name)
+    {
+        //Arrange
+
+        //Act
+        var result = OrderStatus.FromName(name);
+
+        //Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be(name);
+    }
+
+    [Fact]
+    public void ReturnErrorWhenTransportNotFoundById()
+    {
+        //Arrange
+        var id = -1;
+
+        //Act
+        var result = OrderStatus.FromId(id);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnErrorWhenStatusNotFoundByName()
+    {
+        //Arrange
+        var name = "not-existed-order-status";
+
+        //Act
+        var result = OrderStatus.FromName(name);
+
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnListOfStatuses()
+    {
+        //Arrange
+
+        //Act
+        var allStatuses = OrderStatus.List();
+
+        //Assert
+        allStatuses.Should().NotBeEmpty();
+    }
+}
